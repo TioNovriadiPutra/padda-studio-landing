63f587c58d969c9f80a82a927712d3709dc98542
TioNovriadiPutra <tionvriadi@gmail.com>
2025-01-03 03:41:00 +0700

    fix: fix section mobile layout

--- src/components/organism/Section1.tsx
+++ src/components/organism/Section1.tsx
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
   },
   containerMobile: {
     gap: 69.02,
-    height: 1013.5,
+    minHeight: 1013.5,
     paddingHorizontal: 14,
     paddingVertical: 40,
   },
