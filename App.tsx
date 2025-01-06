37e0720d41398f03c4793870fd2c9bb86ef28cb0
TioNovriadiPutra <tionvriadi@gmail.com>
2025-01-06 13:40:01 +0700

    feat: add vercel speed insight

--- App.tsx
+++ App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import AppNav from "AppNav";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 const App = () => {
 	inject();
+	injectSpeedInsights();
 
 	return (
 		<RecoilRoot>
