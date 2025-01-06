4c458bc11c63b3205b5ce9c2d0353d5d6e94f697
TioNovriadiPutra <tionvriadi@gmail.com>
2025-01-06 17:18:49 +0700

    fix: fix layout error on instagram webview

--- src/components/organism/Section2.tsx
+++ src/components/organism/Section2.tsx
@@ -4,49 +4,49 @@ import { colors } from "@themes/colors";
 import { Section2BoxList, Section2Header } from "@components/molecule";
 
 type Props = {
-  trigger: boolean;
-  trigger2: boolean;
-  isMobile: boolean;
-  onLayout: (e: LayoutChangeEvent) => void;
+	trigger: boolean;
+	trigger2: boolean;
+	isMobile: boolean;
+	onLayout: (e: LayoutChangeEvent) => void;
 };
 
 const Section2 = ({ trigger, trigger2, isMobile, onLayout }: Props) => {
-  return (
-    <View
-      style={[
-        styles.container,
-        isMobile ? styles.containerMobile : styles.containerWeb,
-      ]}
-    >
-      <Section2Header
-        trigger={trigger}
-        isMobile={isMobile}
-        onLayout={onLayout}
-      />
+	return (
+		<View
+			style={[
+				styles.container,
+				isMobile ? styles.containerMobile : styles.containerWeb,
+			]}
+		>
+			<Section2Header
+				trigger={trigger}
+				isMobile={isMobile}
+				onLayout={onLayout}
+			/>
 
-      <Section2BoxList
-        trigger={isMobile ? trigger2 : trigger}
-        isMobile={isMobile}
-      />
-    </View>
-  );
+			<Section2BoxList
+				trigger={isMobile ? trigger2 : trigger}
+				isMobile={isMobile}
+			/>
+		</View>
+	);
 };
 
 export default Section2;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors["Neutral-950"],
-    gap: 60,
-  },
-  containerWeb: {
-    height: 1132,
-    paddingHorizontal: 120,
-    paddingVertical: 84,
-  },
-  containerMobile: {
-    height: 1628,
-    paddingHorizontal: 14,
-    paddingVertical: 64,
-  },
+	container: {
+		backgroundColor: colors["Neutral-950"],
+		gap: 60,
+	},
+	containerWeb: {
+		height: 1132,
+		paddingHorizontal: 120,
+		paddingVertical: 84,
+	},
+	containerMobile: {
+		minHeight: 1628,
+		paddingHorizontal: 14,
+		paddingVertical: 64,
+	},
 });
--- src/components/organism/Section6.tsx
+++ src/components/organism/Section6.tsx
@@ -4,33 +4,33 @@ import { colors } from "@themes/colors";
 import { Section6List } from "@components/molecule";
 
 type Props = {
-  scrollPos: number;
-  isMobile: boolean;
+	scrollPos: number;
+	isMobile: boolean;
 };
 
 const Section6 = ({ scrollPos, isMobile }: Props) => {
-  return (
-    <View
-      style={[
-        styles.container,
-        isMobile ? styles.containerMobile : styles.containerWeb,
-      ]}
-    >
-      <Section6List scrollPos={scrollPos} isMobile={isMobile} />
-    </View>
-  );
+	return (
+		<View
+			style={[
+				styles.container,
+				isMobile ? styles.containerMobile : styles.containerWeb,
+			]}
+		>
+			<Section6List scrollPos={scrollPos} isMobile={isMobile} />
+		</View>
+	);
 };
 
 export default Section6;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors["Neutral-50"],
-  },
-  containerWeb: {
-    height: 3300,
-  },
-  containerMobile: {
-    height: 4175,
-  },
+	container: {
+		backgroundColor: colors["Neutral-50"],
+	},
+	containerWeb: {
+		height: 3300,
+	},
+	containerMobile: {
+		minHeight: 4175,
+	},
 });
