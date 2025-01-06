f6a467529914899dcea9e9a9f8e569a0ffea1552
TioNovriadiPutra <tionvriadi@gmail.com>
2025-01-06 13:36:11 +0700

    feat: add vercel analytics

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import AppNav from "AppNav";
+import { inject } from "@vercel/analytics";
 
 const App = () => {
+	inject();
+
 	return (
 		<RecoilRoot>
 			<AppNav />
